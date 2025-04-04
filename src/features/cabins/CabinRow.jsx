afe6061e49f0fa9d8e015a18431e235910c7f755
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-04-04 14:04:27 -0700

    create reusable context menu

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -7,18 +7,7 @@ import useCreateCabin from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
-
-// const TableRow = styled.div`
-//     display: grid;
-//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//     column-gap: 2.4rem;
-//     align-items: center;
-//     padding: 1.4rem 2.4rem;
-
-//     &:not(:last-child) {
-//         border-bottom: 1px solid var(--color-grey-100);
-//     }
-// `;
+import Menus from "../../ui/Menus";
 
 const Img = styled.img`
     display: block;
@@ -57,7 +46,7 @@ const CabinRow = ({ cabin }) => {
         image,
         description,
     } = cabin;
-    const { isCreating, createCabin } = useCreateCabin();
+    const { createCabin } = useCreateCabin();
 
     const { isDeleting, deleteCabin } = useDeleteCabin();
 
@@ -84,32 +73,43 @@ const CabinRow = ({ cabin }) => {
                 <span>&mdash;</span>
             )}
             <div>
-                <button disabled={isCreating} onClick={handleDuplicate}>
-                    <HiSquare2Stack />
-                </button>
-
                 <Modal>
-                    <Modal.Open opens="edit">
-                        <button>
-                            <HiPencil />
-                        </button>
-                    </Modal.Open>
-                    <Modal.Window name="edit">
-                        <CreateCabinForm cabinToEdit={cabin} />
-                    </Modal.Window>
+                    <Menus.Menu>
+                        <Menus.Toggle id={id} />
 
-                    <Modal.Open opens="delete">
-                        <button>
-                            <HiTrash />
-                        </button>
-                    </Modal.Open>
-                    <Modal.Window name="delete">
-                        <ConfirmDelete
-                            resourceName="cabin"
-                            onConfirm={() => deleteCabin(id)}
-                            disabled={isDeleting}
-                        />
-                    </Modal.Window>
+                        <Menus.List id={id}>
+                            <Menus.Button
+                                icon={<HiSquare2Stack />}
+                                onClick={handleDuplicate}
+                            >
+                                Duplicate
+                            </Menus.Button>
+
+                            <Modal.Open opens="edit">
+                                <Menus.Button icon={<HiPencil />}>
+                                    Edit
+                                </Menus.Button>
+                            </Modal.Open>
+
+                            <Modal.Open opens="delete">
+                                <Menus.Button icon={<HiTrash />}>
+                                    Delete
+                                </Menus.Button>
+                            </Modal.Open>
+                        </Menus.List>
+
+                        <Modal.Window name="edit">
+                            <CreateCabinForm cabinToEdit={cabin} />
+                        </Modal.Window>
+
+                        <Modal.Window name="delete">
+                            <ConfirmDelete
+                                resourceName="cabin"
+                                onConfirm={() => deleteCabin(id)}
+                                disabled={isDeleting}
+                            />
+                        </Modal.Window>
+                    </Menus.Menu>
                 </Modal>
             </div>
         </Table.Row>
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 // const Table = styled.div`
 //     border: 1px solid var(--color-grey-200);
@@ -36,21 +37,25 @@ const CabinTable = () => {
     if (isError) return <span>Error: {error.message}</span>;
 
     return (
-        <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-            <Table.Header>
-                <div></div>
-                <div>Cabin</div>
-                <div>Capacity</div>
-                <div>Price</div>
-                <div>Discount</div>
-                <div></div>
-            </Table.Header>
+        <Menus>
+            <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+                <Table.Header>
+                    <div></div>
+                    <div>Cabin</div>
+                    <div>Capacity</div>
+                    <div>Price</div>
+                    <div>Discount</div>
+                    <div></div>
+                </Table.Header>
 
-            <Table.Body
-                data={cabins}
-                render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-            />
-        </Table>
+                <Table.Body
+                    data={cabins}
+                    render={(cabin) => (
+                        <CabinRow cabin={cabin} key={cabin.id} />
+                    )}
+                />
+            </Table>
+        </Menus>
     );
 };
 
